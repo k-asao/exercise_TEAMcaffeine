387db97d88ba6bc88d91c36e583cb7c8009b3ff8
froide-asao <kazutoshi-asao@froide.co.jp>
2021-05-25 18:04:23 +0900

    初期DB設定の変更

--- docker/database/initdb.d/exerciseJava.sql
+++ docker/database/initdb.d/exerciseJava.sql
@@ -106,45 +106,45 @@ ALTER TABLE `employee`
 
 
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('海馬瀬人', 'カイバセト', 1972-04-01, 1, 8, '08024338464', 'emp001@example.co.jp');
+            ('海馬瀬人', 'カイバセト', '1972-04-01', 1, 8, '08024338464', 'emp001@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('麻倉葉', 'アサクラヨウ', 1977-08-01, 2, 8, '08054833414', 'emp002@example.co.jp');
+            ('麻倉葉', 'アサクラヨウ', '1977-08-01', 2, 8, '08054833414', 'emp002@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('ランデル・オーランド', 'ランデル・オーランド', 1976-04-01, 3, 8, '080-7833-0868', 'emp003@example.co.jp');
+            ('ランデル・オーランド', 'ランデル・オーランド', '1976-04-01', 3, 8, '080-7833-0868', 'emp003@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('虎杖悠二', 'イタドリユウジ', 1979-04-01, 3, 8, '08019258645', 'emp004@example.co.jp');
+            ('虎杖悠二', 'イタドリユウジ', '1979-04-01', 3, 8, '08019258645', 'emp004@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('阿良々木暦', 'アララギコヨミ', 1982-04-01, 6, 8, '08081510801', 'emp005@example.co.jp');
+            ('阿良々木暦', 'アララギコヨミ', '1982-04-01', 6, 8, '08081510801', 'emp005@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('立花響', 'タチバナヒビキ', 1984-04-01, 4, 8, '08000320846', 'emp006@example.co.jp');
+            ('立花響', 'タチバナヒビキ', '1984-04-01', 4, 8, '08000320846', 'emp006@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('上条当麻', 'カミジョウトウマ', 1985-04-01, 5, 8, '08012532148', 'emp007@example.co.jp');
+            ('上条当麻', 'カミジョウトウマ', '1985-04-01', 5, 8, '08012532148', 'emp007@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('禅院真希', 'ゼンインマキ', 1986-04-01, 5, 8, '08087327823', 'emp008@example.co.jp');
+            ('禅院真希', 'ゼンインマキ', '1986-04-01', 5, 8, '08087327823', 'emp008@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('比企谷八幡', 'ヒキガヤハチマン', 1986-04-01, 7, 8, '08097651275', 'emp009@example.co.jp');
+            ('比企谷八幡', 'ヒキガヤハチマン', '1986-04-01', 7, 8, '08097651275', 'emp009@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('槙島聖護', 'マキシマショウゴ', 1992-04-01, 4, 8, '08078322964', 'emp010@example.co.jp');
+            ('槙島聖護', 'マキシマショウゴ', '1992-04-01', 4, 8, '08078322964', 'emp010@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('月ノ美兎', 'ツキノミト', 1993-04-01, 4, 8, '08019728365', 'emp011@example.co.jp');
+            ('月ノ美兎', 'ツキノミト', '1993-04-01', 4, 8, '08019728365', 'emp011@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('黒崎一護', 'クロサキイチゴ', 2002-04-01, 8, 3, '08087631221', 'emp012@example.co.jp');
+            ('黒崎一護', 'クロサキイチゴ', '2002-04-01', 8, 3, '08087631221', 'emp012@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('爆轟克己', 'バクゴウカツキ', 2003-04-01, 6, 6, '08014513243', 'emp013@example.co.jp');
+            ('爆轟克己', 'バクゴウカツキ', '2003-04-01', 6, 6, '08014513243', 'emp013@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('工藤新一', 'クドウシンイチ', 2006-04-01, 6, 5, '08048541452', 'emp014@example.co.jp');
+            ('工藤新一', 'クドウシンイチ', '2006-04-01', 6, 5, '08048541452', 'emp014@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('笠原郁', 'カサハライク', 2008-04-01, 8, 3, '08067337634', 'emp015@example.co.jp');
+            ('笠原郁', 'カサハライク', '2008-04-01', 8, 3, '08067337634', 'emp015@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('岡部倫太郎', 'オカベリンタロウ', 2012-04-01, 9, 8, '08094628466', 'emp015@example.co.jp');
+            ('岡部倫太郎', 'オカベリンタロウ', '2012-04-01', 9, 8, '08094628466', 'emp015@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('有馬公生', 'アリマコウセイ', 2014-04-01, 9, 5, '08027452947', 'emp015@example.co.jp');
+            ('有馬公生', 'アリマコウセイ', '2014-04-01', 9, 5, '08027452947', 'emp015@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('金木研', 'カネキケン', 2017-04-01, 10, 1, '08000388462', 'emp015@example.co.jp');
+            ('金木研', 'カネキケン', '2017-04-01', 10, 1, '08000388462', 'emp015@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('夏目貴志', 'ナツメタカシ', 2020-04-01, 10, 3, '08027551836', 'emp015@example.co.jp');
+            ('夏目貴志', 'ナツメタカシ', '2020-04-01', 10, 3, '08027551836', 'emp015@example.co.jp');
 INSERT INTO employee(name, kana, hire_date, post_id, dept_id, tel, email) VALUES 
-            ('刹那・F・セイエイ', 'セキザワキクジ', 2021-04-01, 10, 6, '08064728912', 'emp015@example.co.jp');
+            ('関沢菊治', 'セキザワキクジ', '2021-04-01', 10, 6, '08064728912', 'emp015@example.co.jp');
 
 COMMIT;
 
