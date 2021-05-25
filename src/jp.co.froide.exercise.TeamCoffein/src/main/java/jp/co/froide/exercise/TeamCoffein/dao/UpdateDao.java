e8b08247891a64e6f31f67d1d11d48b0d9b89f5c
froide-asao <kazutoshi-asao@froide.co.jp>
2021-05-25 15:04:18 +0900

    更新/削除機能の追加

---
+++ src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/dao/UpdateDao.java
@@ -0,0 +1,21 @@
+package jp.co.froide.exercise.TeamCoffein.dao;
+
+import jp.co.froide.exercise.TeamCoffein.entity.Employee;
+import org.seasar.doma.*;
+import org.seasar.doma.boot.ConfigAutowireable;
+
+@ConfigAutowireable
+@Dao
+public interface UpdateDao {
+
+    @Sql("select /*%expand*/* from employee where emp_id = /*id*/0")
+    @Select
+    Employee selectEmpByID(Integer id);
+
+
+    @Update
+    int update(Employee employee);
+
+    @Delete
+    int delete(Employee employee);
+}
