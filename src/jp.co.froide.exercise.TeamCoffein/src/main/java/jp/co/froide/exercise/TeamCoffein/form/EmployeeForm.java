a1a67e9d35f536756f43c28a35ea3914e41795bc
froide-asao <kazutoshi-asao@froide.co.jp>
2021-05-26 09:50:10 +0900

    氏名、カナに文字数制限のバリデーションを追加

--- src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/form/EmployeeForm.java
+++ src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/form/EmployeeForm.java
@@ -1,6 +1,7 @@
 package jp.co.froide.exercise.TeamCoffein.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -11,9 +12,11 @@ import java.io.Serializable;
 @Data
 public class EmployeeForm implements Serializable {
     public static final long serialVersionUID= 1L;
+    @Length(min=0, max=255)
     @NotBlank
     String name;
 
+    @Length(min=0, max=255)
     @Pattern(regexp = "[ァ-タダ-ヶー]+", message = "全角カナで入力してください")
     String kana;
 
@@ -29,6 +32,7 @@ public class EmployeeForm implements Serializable {
     @Pattern(regexp = "^0\\d{10,11}$", message = "半角数字で電話番号を入力してください。")
     String tel;
 
+    @Length(min=0, max=255)
     @Email
     @NotBlank
     String email;
