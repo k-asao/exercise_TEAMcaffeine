dc27d763b9aac303474a1cbacb27ee42ecfb6791
“yamato-dejima” <yamato-dejima@froide.co.jp>
2021-06-01 16:55:42 +0900

    add:メールアドレス確認画面、パスワード変更画面にCSSを適用
    メールアドレス確認画面とパスワード変更画面にCSSを適用。また、ログイン画面を含めた上記２画面にキャンセルボタンとその機能を追加した

--- src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/controller/LoginController.java
+++ src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/controller/LoginController.java
@@ -18,10 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -29,7 +26,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
     @Autowired
-    EmployeeDao  empDao;
+    EmployeeDao empDao;
     @Autowired
     UpdateDao updateDao;
     @Autowired
@@ -42,14 +39,18 @@ public class LoginController {
     MailFormValidator mailFormValidator;
 
     @InitBinder("confPassForm")
-    public void validatorBinder(WebDataBinder webDataBinder){ webDataBinder.addValidators(passFormValidator);}
+    public void validatorBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(passFormValidator);
+    }
 
     @InitBinder("confMailForm")
-    public void validatorBinder2(WebDataBinder webDataBinder){webDataBinder.addValidators(mailFormValidator);}
+    public void validatorBinder2(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(mailFormValidator);
+    }
 
     @GetMapping("/emp/login")
-    public String showLoginPage(@ModelAttribute LoginForm form, Model model){
-        if(!model.containsAttribute("message")){
+    public String showLoginPage(@ModelAttribute LoginForm form, Model model) {
+        if (!model.containsAttribute("message")) {
             model.addAttribute("message", null);
         }
         model.addAttribute("loginForm", new LoginForm());
@@ -57,48 +58,47 @@ public class LoginController {
     }
 
     @PostMapping("/emp/login")
-    public String index(@Validated @ModelAttribute LoginForm form, BindingResult result){
-        if(result.hasErrors()){
+    public String index(@Validated @ModelAttribute LoginForm form, BindingResult result) {
+        if (result.hasErrors()) {
             System.out.println("BBBBBBBB");
             return "login";
         }
         System.out.println("aaaaaa");
-        return "forward:" + "/processing" ;
+        return "forward:" + "/processing";
     }
 
     @PostMapping("/success")
-    public String loginSuccess(@ModelAttribute LoginForm form, RedirectAttributes ra){
+    public String loginSuccess(@ModelAttribute LoginForm form, RedirectAttributes ra) {
         ra.addFlashAttribute("message", "login_success");
         return "redirect:/emp";
     }
 
     @GetMapping("/loginFail")
-    public String loginFailure(@ModelAttribute LoginForm form, Model model, RedirectAttributes ra){
+    public String loginFailure(@ModelAttribute LoginForm form, Model model, RedirectAttributes ra) {
         ra.addFlashAttribute("message", "ログインIDもしくはパスワードが違います。");
         return "redirect:/emp/login";
     }
 
     @GetMapping("/emp/forgetPass")
-    public String forgetPass(Model model){
-        if(!model.containsAttribute("error")) {
+    public String forgetPass(Model model) {
+        if (!model.containsAttribute("error")) {
             model.addAttribute("mailForm", new ConfMailForm());
         }
         return "PassConfMail";
     }
 
     @GetMapping("/emp/changePass")
-    public String showPassForm(Model model){
-        if(model.containsAttribute("error")){
+    public String showPassForm(Model model) {
+        if (model.containsAttribute("error")) {
             return "changePass";
-        }else {
+        } else {
             return "redirect:/emp/login";
         }
     }
 
-
     @PostMapping("/emp/changePass")
-    public String changePass(@Validated @ModelAttribute ConfMailForm form, BindingResult result, Model model, RedirectAttributes ra){
-        if(result.hasErrors()){
+    public String changePass(@Validated @ModelAttribute ConfMailForm form, BindingResult result, Model model, RedirectAttributes ra) {
+        if (result.hasErrors()) {
             ra.addFlashAttribute("org.springframework.validation.BindingResult.mailForm", result);
             ra.addFlashAttribute("error", "this has errors");
             ra.addFlashAttribute("mailForm", form);
@@ -114,8 +114,8 @@ public class LoginController {
     }
 
     @PostMapping("/emp/sucChange")
-    public String successChangePass(@Validated @ModelAttribute ConfPassForm form, BindingResult result, Model model, RedirectAttributes ra){
-        if(result.hasErrors()){
+    public String successChangePass(@Validated @ModelAttribute ConfPassForm form, BindingResult result, Model model, RedirectAttributes ra) {
+        if (result.hasErrors()) {
             ra.addFlashAttribute("org.springframework.validation.BindingResult.passForm", result);
             ra.addFlashAttribute("error", "this has errors");
             ra.addFlashAttribute("email", form.getEmail());
@@ -129,5 +129,8 @@ public class LoginController {
         return "redirect:/emp/login";
     }
 
-
+    @RequestMapping(value = {"/emp/login", "/emp/changePass", "/emp/sucChange"}, params = "action=cancel")
+    public String cancelButton() {
+        return "redirect:/emp";
+    }
 }
