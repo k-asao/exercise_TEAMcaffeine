2d706b048804e81703ec1d7ddbf8ba9094038add
froide-asao <kazutoshi-asao@froide.co.jp>
2021-06-01 17:22:44 +0900

    add: 電話番号、メアドに関するバリデーション
    それらが既に登録されている場合のバリデーションをEmpFormValidatorとして追加

--- src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/validation/EmpFormValidator.java
+++ src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/validation/EmpFormValidator.java
@@ -1,24 +1,35 @@
-/*package jp.co.froide.exercise.TeamCoffein.validation;
+package jp.co.froide.exercise.TeamCoffein.validation;
 
 
+import jp.co.froide.exercise.TeamCoffein.dao.EmployeeDao;
+import jp.co.froide.exercise.TeamCoffein.entity.Post;
+import jp.co.froide.exercise.TeamCoffein.entity.PostEmployee;
 import jp.co.froide.exercise.TeamCoffein.form.EmployeeForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
 public class EmpFormValidator implements Validator {
+    @Autowired
+    EmployeeDao empDao;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return EmployeeForm.class.isAssignableFrom(clazz);
     }
     @Override
     public void validate(Object target, Errors errors) {
-//        EmployeeForm empForm = (EmployeeForm) target;
-//        if (!EmployeeForm.password.equals(EmployeeForm.passwordConfirm)) {
-//            errors.rejectValue("passwordConfirm", "UserFormValidator.userForm.passwordConfirm","これがエラ〜メッセージですよ。");
-//        }
+        EmployeeForm empForm = (EmployeeForm) target;
+        PostEmployee empByEmail = empDao.selectByEmail(empForm.getEmail());
+        PostEmployee empByTel = empDao.selectByTel(empForm.getTel());
+
+        if(empByEmail != null){
+            errors.rejectValue("email", "EmployeeForm.Email.AlreadyExist");
+        }
+        if(empByTel != null){
+            errors.rejectValue("tel", "EmployeeForm.Tel.AlreadyExist");
+        }
     }
-}*/
-
-
+}
