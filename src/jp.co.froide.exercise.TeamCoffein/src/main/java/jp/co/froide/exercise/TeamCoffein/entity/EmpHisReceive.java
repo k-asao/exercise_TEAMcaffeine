d6fc77a2a49991edcc32588bee60ec37d49c0ac3
froide-asao <kazutoshi-asao@froide.co.jp>
2021-06-02 15:45:20 +0900

    add: 変更履歴確認機能の追加
    変更履歴の確認、20件超えた際の物理削除を追加

---
+++ src/jp.co.froide.exercise.TeamCoffein/src/main/java/jp/co/froide/exercise/TeamCoffein/entity/EmpHisReceive.java
@@ -0,0 +1,24 @@
+package jp.co.froide.exercise.TeamCoffein.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.seasar.doma.Entity;
+import org.seasar.doma.Metamodel;
+import org.seasar.doma.Table;
+
+@Table(name = "emp_history")
+@Entity(metamodel = @Metamodel)
+@Getter
+@Setter
+public class EmpHisReceive {
+
+    Integer emp_id;
+    String name;
+    String kana;
+    String hire_date;
+    String post_name;
+    String dept_name;
+    String tel;
+    String email;
+    String insert_history_at;
+}
