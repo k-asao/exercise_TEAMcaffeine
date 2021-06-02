abc32f8eada4c98d7b700a9b35497b2ed619f169
k-asao <81937785+k-asao@users.noreply.github.com>
2021-06-02 09:20:09 +0900

    Update exerciseJava.sql

--- docker/database/initdb.d/exerciseJava.sql
+++ docker/database/initdb.d/exerciseJava.sql
@@ -83,7 +83,7 @@ CREATE TABLE `employee` (
   `tel` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL UNIQUE,
   `email` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL UNIQUE,
   `password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `delete_flag` int(2) NOT NULL,
+  `delete_flag` int(2) NOT NULL DEFAULT 0,
   `create_at` datetime COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `update_at` datetime COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   FOREIGN KEY fk_post(post_id) REFERENCES post(post_id),
